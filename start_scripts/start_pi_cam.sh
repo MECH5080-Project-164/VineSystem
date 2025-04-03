bb1f80e9572e39d2c82a3c4b123c1c36080098e5
Sam Wilson <el20sw@leeds.ac.uk>
2025-04-03 11:14:39 +0100

    basic script to start the Pi Camera ROS Node

--- start_scripts/start_pi_cam.sh
+++ start_scripts/start_pi_cam.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+# Verify that we're in the Vine Container
+if [ -z "$VINE_CONTAINER" ]; then
+    echo "This script must be run inside the Vine Container."
+    exit 1
+fi
+
+source /opt/ros/"$ROS_DISTRO"/setup.bash
+source /picam/install/setup.bash
+
+ros2 run camera_ros camera_node
