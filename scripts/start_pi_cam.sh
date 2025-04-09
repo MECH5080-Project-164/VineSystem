23568b835ce0b9e77eba99e5f45a4ea70421a052
Sam Wilson <el20sw@leeds.ac.uk>
2025-04-09 18:17:41 +0100

    script to start Pi Cameras accepts camera index as argument

--- scripts/start_pi_cam.sh
+++ scripts/start_pi_cam.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# --- Functions ---
+function usage() {
+    echo "Usage: $0 [camera_index]"
+    echo "  camera_index: The index of the camera to start (optional)."
+}
+
+# --- Main Script ---
+
 # Verify that we're in the Vine Container
 if [ -z "$VINE_CONTAINER" ]; then
     echo "This script must be run inside the Vine Container."
@@ -9,4 +17,15 @@ fi
 source /opt/ros/"$ROS_DISTRO"/setup.bash
 source /picam/install/setup.bash
 
-ros2 run camera_ros camera_node
+COMMAND="ros2 run camera_ros camera_node"
+CAMERA_INDEX=${1}
+
+# if no camera index is provided, we hand over to the camera node by running the stem command
+if [ -z "$CAMERA_INDEX" ]; then
+    echo "No camera index provided. Starting camera node with default index."
+    $COMMAND
+else
+    # if a camera index is provided, we run the command with the camera index
+    echo "Starting camera node with index $CAMERA_INDEX."
+    $COMMAND --ros-args -p camera:=$CAMERA_INDEX
+fi
