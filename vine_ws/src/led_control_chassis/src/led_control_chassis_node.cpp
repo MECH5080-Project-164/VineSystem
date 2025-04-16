cf92b55ac6567fa52c60108f8875386a6b73f3d8
Sam Wilson <el20sw@leeds.ac.uk>
2025-04-16 15:23:03 +0100

    Parameterise for pins, clock and range for PWM

--- vine_ws/src/led_control_chassis/src/led_control_chassis_node.cpp
+++ vine_ws/src/led_control_chassis/src/led_control_chassis_node.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
 #include <rclcpp/rclcpp.hpp>
@@ -12,6 +13,13 @@ public:
   using ChassisLed = vine_interfaces::msg::ChassisLed;
 
   LedControlChassis() : Node("led_control_chassis") {
+    // Declare the node parameters
+    auto pwm_pins_desc = rcl_interfaces::msg::ParameterDescriptor();
+    pwm_pins_desc.description = "List of GPIO pins (BCM) for PWM output";
+    this->declare_parameter<std::vector<int64_t>>("pwm_pins", {12}, pwm_pins_desc);
+    this->declare_parameter<int64_t>("pwm_range", 1024);
+    this->declare_parameter<int64_t>("pwm_clock", 1);
+
     // Create a subscriber to the "chassis_led_control" topic
     subscription_ = this->create_subscription<ChassisLed>(
       "chassis_led_control", 10,
@@ -28,13 +36,31 @@ public:
     RCLCPP_INFO(this->get_logger(), "wiringPi initialized successfully");
 
     // Configure GPIO pins
-    pinMode(18, PWM_OUTPUT); // Set GPIO BMC pin 18 as PWM output
-    pwmSetMode(PWM_MODE_MS); // Set PWM mode to mark-space
-    pwmSetRange(1024); // Set PWM range to 1024
-    pwmSetClock(1); // Set the PWM clock divisor to 1 - 19.2kHz
-    pwmWrite(18, 0); // Initialize PWM value to 0
-    RCLCPP_INFO(this->get_logger(), "GPIO pins configured successfully");
-  }
+    std::vector<int64_t> pwm_pins_64 = this->get_parameter("pwm_pins").as_integer_array();
+    // Convert int64_t to native int
+    for (const auto& pin_64 : pwm_pins_64) {
+      this->pwm_pins_.push_back(static_cast<int>(pin_64));
+    }
+
+    // Set up the GPIO pins for PWM output
+    for (const auto& pin : this->pwm_pins_) {
+      pinMode(pin, PWM_OUTPUT);
+      RCLCPP_INFO(this->get_logger(), "Configured GPIO pin %d for PWM output", pin);
+    }
+    // Set the PWM mode, range, and clock
+    this->pwm_range = static_cast<int>(this->get_parameter("pwm_range").as_int());
+    this->pwm_clock = static_cast<int>(this->get_parameter("pwm_clock").as_int());
+
+    if (pwm_range <= 0 || pwm_clock <= 0) {
+      RCLCPP_ERROR(this->get_logger(), "Invalid PWM range or clock value");
+      return;
+    }
+
+    pwmSetMode(PWM_MODE_MS);
+    pwmSetRange(this->pwm_range);
+    pwmSetClock(this->pwm_clock);
+
+    }
 
 private:
   void led_control_callback(const ChassisLed::SharedPtr msg) {
@@ -44,7 +70,6 @@ private:
 
   void configure_pwm(const int brightness) {
 
-   int pwm_range = 1024; // Set the PWM range
     if (brightness < 0 || brightness > 100) {
       RCLCPP_ERROR(this->get_logger(), "Brightness value out of range: %d", brightness);
       return;
@@ -52,10 +77,14 @@ private:
     RCLCPP_INFO(this->get_logger(), "Setting brightness to %d", brightness);
 
     // Set the PWM value based on the brightness level
-    int pwm_value = (brightness * pwm_range) / 100; // Scale to 0-1023
-    pwmWrite(18, pwm_value); // Write to GPIO BCM pin 18
+    int pwm_value = (brightness * this->pwm_range) / 100; // Scale to 0-1023
+    for (const auto& pin : this->pwm_pins_) {
+      pwmWrite(pin, pwm_value);
+      RCLCPP_INFO(this->get_logger(), "Set GPIO pin %d to PWM value %d", pin, pwm_value);
+    }
   }
 
+#ifdef USE_SCRIPT
   void call_script(const int brightness) {
     // Construct the command to call the script
     std::string command = "bash /home/workspace/demo.sh " + std::to_string(brightness);
@@ -69,8 +98,12 @@ private:
       RCLCPP_ERROR(this->get_logger(), "Script execution failed with code %d", result);
     }
   }
+#endif  // USE_SCRIPT
 
   rclcpp::Subscription<ChassisLed>::SharedPtr subscription_;
+  std::vector<int> pwm_pins_; // Store the GPIO pins for PWM output
+  int pwm_range; // Store the PWM range
+  int pwm_clock; // Store the PWM clock
 
 };
 
