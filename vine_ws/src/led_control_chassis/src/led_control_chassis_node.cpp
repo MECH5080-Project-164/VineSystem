17f62a5dbfbaf8c1324771c4eef07fa3fa4623b8
Sam Wilson <el20sw@leeds.ac.uk>
2025-04-16 14:38:44 +0100

    Adding wiringPi support for hardware PWM in Node

--- vine_ws/src/led_control_chassis/src/led_control_chassis_node.cpp
+++ vine_ws/src/led_control_chassis/src/led_control_chassis_node.cpp
@@ -4,6 +4,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 
+#include "wiringPi.h"
 #include "vine_interfaces/msg/chassis_led.hpp"
 
 class LedControlChassis : public rclcpp::Node {
@@ -18,18 +19,49 @@ public:
 
     RCLCPP_INFO(this->get_logger(), "LedControlChassis node has been started.");
     RCLCPP_INFO(this->get_logger(), "Waiting for messages on 'chassis_led_control' topic...");
+
+    // Initialize wiringPi
+    if (wiringPiSetupPinType(WPI_PIN_BCM) == -1) {
+      RCLCPP_ERROR(this->get_logger(), "Failed to initialize wiringPi");
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "wiringPi initialized successfully");
+
+    // Configure GPIO pins
+    pinMode(18, PWM_OUTPUT); // Set GPIO BMC pin 18 as PWM output
+    pwmSetMode(PWM_MODE_MS); // Set PWM mode to mark-space
+    pwmSetRange(1024); // Set PWM range to 1024
+    pwmSetClock(1); // Set the PWM clock divisor to 1 - 19.2kHz
+    pwmWrite(18, 0); // Initialize PWM value to 0
+    RCLCPP_INFO(this->get_logger(), "GPIO pins configured successfully");
   }
 
 private:
   void led_control_callback(const ChassisLed::SharedPtr msg) {
     RCLCPP_INFO(this->get_logger(), "Received: '%d'", msg->brightness);
-    call_script(std::to_string(msg->brightness));
+    configure_pwm(msg->brightness);
   }
 
-  void call_script(const std::string& args) {
+  void configure_pwm(const int brightness) {
+
+   int pwm_range = 1024; // Set the PWM range
+    if (brightness < 0 || brightness > 100) {
+      RCLCPP_ERROR(this->get_logger(), "Brightness value out of range: %d", brightness);
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "Setting brightness to %d", brightness);
+
+    // Set the PWM value based on the brightness level
+    int pwm_value = (brightness * pwm_range) / 100; // Scale to 0-1023
+    pwmWrite(18, pwm_value); // Write to GPIO BCM pin 18
+  }
+
+  void call_script(const int brightness) {
     // Construct the command to call the script
-    std::string command = "bash /home/workspace/demo.sh " + args;
+    std::string command = "bash /home/workspace/demo.sh " + std::to_string(brightness);
     int result = system(command.c_str());
+
+    RCLCPP_INFO(this->get_logger(), "Brightness value sent to script: %d", brightness);
 
     if (result == 0) {
       RCLCPP_INFO(this->get_logger(), "Script executed successfully");
