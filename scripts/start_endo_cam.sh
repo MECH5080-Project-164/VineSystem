95f5d96aeadbb3097d220ba4d18e085d8d9815db
Sam Wilson <el20sw@leeds.ac.uk>
2025-04-24 16:58:12 +0100

    change permissions and populate start_endo_cam

--- scripts/start_endo_cam.sh
+++ scripts/start_endo_cam.sh
@@ -5,3 +5,12 @@ if [ -z "$VINE_CONTAINER" ]; then
     echo "This script must be run inside the Vine Container."
     exit 1
 fi
+
+PARAM_FILE="/home/workspace/students/acutiy_code/pharos_ws/src/pharos_ros/config/params_1.yaml"
+
+if [ ! -f "$PARAM_FILE" ]; then
+    echo "Default parameter file not found: $PARAM_FILE"
+    exit 1
+fi
+
+ros2 run usb_cam usb_cam_node_exe --ros-args --params-file "$PARAM_FILE"
