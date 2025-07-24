baad25ac51b88a47c236edaebd859cb75adb9dc2
Sam Wilson <el20sw@leeds.ac.uk>
2025-07-24 11:33:45 +0100

    Add dependencies to configuration

--- vine_ws/src/gravity_pressure_sensor/setup.py
+++ vine_ws/src/gravity_pressure_sensor/setup.py
@@ -11,7 +11,12 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'smbus',
+        'spidev',
+        'RPi.GPIO'
+    ],
     zip_safe=True,
     maintainer='root',
     maintainer_email='root@todo.todo',
